b4904125a9f7e8fde9aaa918a2af4f537b1144b5
Harshithaveeresh <bvharshitha.klg@gmail.com>
2026-03-10 20:35:05 +0530

    Update streamlit_app.py
    u 6

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,11 +29,12 @@ ingredients_list = st.multiselect(
 
 # New section to display smoothiefroot nutrition information
 import requests
-
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 
 # Show JSON data instead of <Response [200]>
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
 if ingredients_list:
     ingredients_string = ''
