9d25136a36d459956fee8fe5a51439515d634abc
Harshithaveeresh <bvharshitha.klg@gmail.com>
2026-03-11 01:42:36 +0530

    Update streamlit_app.py
    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,8 +40,15 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit" + fruit_chosen)
-        st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+
+# Safe JSON parse (matches workshop output)
+        try:
+          st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        except:
+          st.info("Sorry, that fruit is not in the Smoothiefroot database.")
+
 
   
 
